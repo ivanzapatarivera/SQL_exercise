9d36a8ea18ed9cacdc885415f5669f7e5daa4c87
Ivan Zapata <falenyo@gmail.com>
2021-03-26 19:33:57 -0400

    Inserted raw data into table.

--- problem2.sql
+++ problem2.sql
@@ -1,8 +1,21 @@
 DROP TABLE IF EXISTS item_details;
 
 CREATE TABLE item_details (
-    item_id INT AUTO_INCREMENT NOT NULL,
+    item_id VARCHAR(10) NOT NULL,
     item_price FLOAT NOT NULL, 
     item_description VARCHAR(50) NOT NULL,
     Primary Key (item_id)
 );
+
+DROP TABLE item_details;
+
+INSERT INTO item_details
+VALUES
+    ('itm001',2,'Avocado (ind)'),
+    ('itm002',60,'Apple Bag'),
+    ('itm003',0.5,'Lemons (ind)'),
+    ('itm004',2,'Banana (lb)'),
+    ('itm005',3,'Broccoli (head)')
+;
+
+SELECT * FROM item_details;
