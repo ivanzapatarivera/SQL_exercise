b4870d91360e940f0df53c172d65bfd4f0f26a98
Ivan Zapata <falenyo@gmail.com>
2021-03-26 11:42:44 -0400

    created table

--- problem1.sql
+++ problem1.sql
@@ -2,3 +2,10 @@ Select @@autocommit;
 Set @@autocommit;
 Select @@SQL_SAFE_UPDATES;
 Set @@SQL_SAFE_UPDATES = 0;
+
+CREATE TABLE IF NOT EXISTS animals (
+    ID INT Auto_Increment Not Null,
+    Animal_Name VARCHAR(50) Not Null,
+    Animal_Age INT(3) Not Null,
+    Primary Key (ID)
+);
