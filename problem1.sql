b79b453e78fbd6e9e6d06135a6afd35ebf772df2
Ivan Zapata <falenyo@gmail.com>
2021-03-30 01:09:21 -0400

    Steps from 1 to 5 OK

--- problem1.sql
+++ problem1.sql
@@ -7,6 +7,7 @@ DROP SCHEMA IF EXISTS da_bootcamp_zapatarivera;
 CREATE SCHEMA IF NOT EXISTS da_bootcamp_zapatarivera;
 Use da_bootcamp_zapatarivera;
 
+-- 1. Create a new table 'animals' with an ID column (type INT) and 2 additional columns (any name and datatype).
 CREATE TABLE IF NOT EXISTS animals (
     ID INT Auto_Increment Not Null,
     Animal_Name VARCHAR(50) Not Null,
@@ -14,6 +15,7 @@ CREATE TABLE IF NOT EXISTS animals (
     Primary Key (ID)  
 );
 
+-- 2. Add a new column to the table, then DROP one of the other columns (other than ID).
 ALTER TABLE animals
 ADD COLUMN 
     Animal_Class VARCHAR(50) Not Null,
@@ -22,10 +24,13 @@ DROP COLUMN
 DROP COLUMN
     Animal_Age;
 
+-- 3. Rename the table to 'zoo_animals'
 ALTER TABLE animals
 RENAME zoo_animals;
 
+-- 4. Select all records from the (empty) zoo_animals table
 SELECT * FROM zoo_animals;
 
+-- 5. Drop the table - actually, to be on the safe side, use the "if exists" condition to drop animals if it exists, and drop zoo_animals if it exists.
 DROP TABLE IF EXISTS animals;
 DROP TABLE IF EXISTS zoo_animals;
