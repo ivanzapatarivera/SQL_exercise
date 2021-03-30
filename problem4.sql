3496e5fced1c5752c7679e79331c5c8cdb81b35f
Ivan Zapata <falenyo@gmail.com>
2021-03-30 13:53:31 -0400

    Steps 1 to 4 OK

--- problem4.sql
+++ problem4.sql
@@ -2,26 +2,31 @@
 -- in order to create the INSERT statement. Comment below with a description of what EXCEL
 -- functions you used to create the data, copy-pasting the entire functions including the = sign
 
--- 	Entered dates 2019-07-03 in cell A1 and 2019-12-31 in cell A2.
--- 	Entered this formula in cell B1: =A1 + 1
--- 	Entered this formula in cell B2: =IF(B1 + 1 < $A$2, B1 + 1, "")
--- 	Autofilled formula in B2 down to cells B2 to B182.
--- 	Created mask with this formula in cell C1: =TEXT(B1,"('YYYY-MM-DD'),")
--- 	Autofilled formula in C2 down to cells C2 to C182.
+-- 	FUNCTIONS USED
+-- 		=A1 + 1
+-- 		=IF(B1 + 1 < $A$2, B1 + 1, "")
+-- 		=TEXT(B1,"('YYYY-MM-DD'),")
+
+-- STEPS
+-- 		Entered dates 2019-07-03 in cell A1 and 2019-12-31 in cell A2.
+-- 		Entered =A1 + 1 in cell B1.
+-- 		Entered =IF(B1 + 1 < $A$2, B1 + 1, "") in cell B2.
+-- 		Autofilled function in B2 down to cells B2 to B182.
+-- 		Created mask with =TEXT(B1,"('YYYY-MM-DD'),") function in cell C1.
+-- 		Autofilled function in C2 down to cells C2 to C182.
 
 use da_bootcamp_zapatarivera;
 
 DROP TABLE IF EXISTS date_dim;
-
 CREATE TABLE IF NOT EXISTS date_dim (
 	TheDate date PRIMARY KEY,
     TheYear int,
     TheMonth int,
-    TheMonthName varchar(9), #January February March etc
+    TheMonthName varchar(9), 
     TheDay int,
     TheDayOfWeek int,
-    TheDayOfWeekName varchar(9), #Sunday Monday Tuesday etc
-    YYYYQQ char(6) #2019Q3, 2019Q4
+    TheDayOfWeekName varchar(9),
+    YYYYQQ char(6)
 );
 
 UPDATE date_dim 
@@ -37,25 +42,42 @@ set
 
 SELECT * FROM date_dim;
 
--- INSERT INTO data_dim (The Date VALUES
+/*** 
+	INSERT into date_dim only column date_full. A few lines are provided.
+	You must use Excel to generate the data. HINT: Use TEXT to convert a date value to YYYY-MM-DD, 
+	then concatenate to get ('YYYY-MM-DD'), format
+***/
+-- INSERT INTO data_dim (The Date) VALUES
 -- ('2019-07-01'),
 -- ('2019-07-02'),
 -- ('2019-07-03'),
 -- ...
 -- ('2019-12-31')
--- Query is avaliable in file 'problem_seeder.sql'
+-- Query to insert records into data_dim table is avaliable in file 'problem4_seeder.sql'
 
 -- 	3. Sales ($) for each item_description broken down by day of the week.
 -- 	Columns should be: Item Description, Day of Week, Sales Total
-SELECT item_description, TheDayOfWeekName, CONCAT('$ ', SUM(quantity * item_price)) AS total
+SELECT 	item_description AS 'Item Description', 
+		TheDayOfWeekName AS 'Day of Week', 
+        CONCAT('$ ', SUM(quantity * item_price)) AS 'Sales Total'
 	FROM item_details
     LEFT JOIN sales_orders
-    ON item_details.item_id = sales_orders.item_id
+		ON item_details.item_id = sales_orders.item_id
     LEFT JOIN date_dim 
-    ON sales_orders.order_date = date_dim.TheDate
+		ON sales_orders.order_date = date_dim.TheDate
     GROUP BY item_description, TheDayOfWeekName
     ORDER BY item_description, TheDayOfWeek
 	;   
 
-    
-SELECT DISTINCT(TheDayOfWeekName) FROM date_dim;
+-- 4. Total Quantity Sold by Product by Quarter. Columns should be: Item_ID, Quarter YYYYMM, Total Quantity
+SELECT 	item_details.item_id AS 'Item_ID', 
+		YYYYQQ AS 'Quarter YYYYMM', 
+        SUM(quantity) AS 'Total Quantity'
+	FROM item_details
+    LEFT JOIN sales_orders
+		ON item_details.item_id = sales_orders.item_id
+	LEFT JOIN date_dim
+		ON sales_orders.order_date = date_dim.TheDate
+	GROUP BY item_id, YYYYQQ
+    ORDER BY item_id, YYYYQQ
+    ;
